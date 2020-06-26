7b1283f5b20748cbb715c2116eb03d3bcfe5daa5
Caseay <23289746+Caseay@users.noreply.github.com>
2020-06-26 19:59:57 +0800

    first commit

---
+++ src/main/java/org/whispin/drivers/DriversApplication.java
@@ -0,0 +1,13 @@
+package org.whispin.drivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriversApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/whispin/drivers/DriversApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whispin.drivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
